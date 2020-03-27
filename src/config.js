3deebe42f4bb6f36b2b001f874dc871ae4046d1c
Eirikur Nilsson <eirikur@nilsson.is>
2020-03-27 13:52:37 +0000

    Switch to prod environment

--- src/config.js
+++ src/config.js
@@ -1,15 +1,7 @@
-import Constants from 'expo-constants';
-
 function getConfig() {
   return {
-    covidApiUrl: 'https://iceland-covid-tracker-api.herokuapp.com',
+    covidApiUrl: 'https://api.tracking.covid.is',
   };
-
-  // const { packagerOpts } = Constants.manifest;
-  // const environment =
-  //   packagerOpts && packagerOpts.dev ? 'development' : 'production';
-  //
-  // return Constants.manifest.extra[environment];
 }
 
 export default getConfig();
