f99690f52e831876e9fdf92532b752e2df056eba
Aegir Thorsteinsson <aegir@thorsteinsson.is>
2020-03-29 02:00:04 +0000

    Reset badge count when opening data request

--- src/router/logged-in/screens/HomeScreen.js
+++ src/router/logged-in/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { AppState, AppStateStatus, ScrollView } from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import * as Permissions from 'expo-permissions';
 import { UserContext } from '../../../context/user';
 import PropTypes from 'prop-types';
@@ -104,6 +105,9 @@ const HomeScreen = ({ navigation, logout }) => {
       setLoadingUserData(false);
 
       if (user && user.dataRequested) {
+        // Reset badge on app icon
+        PushNotificationIOS.setApplicationIconBadgeNumber(0);
+
         resetStack(navigation, 'RequestData');
       }
 
