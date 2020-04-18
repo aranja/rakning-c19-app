46dc8df6ddf2d9a79be82d131377030f8a5213ed
Eirikur Nilsson <eirikur@nilsson.is>
2020-04-18 00:46:19 +0000

    Fix upgrade receiver.
    Now it should restart tracking with the geolocation foreground service.

--- android/app/src/main/java/is/landlaeknir/rakning/receivers/AppUpgraded.java
+++ android/app/src/main/java/is/landlaeknir/rakning/receivers/AppUpgraded.java
@@ -3,12 +3,20 @@ package is.landlaeknir.rakning.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import com.marianhello.bgloc.service.LocationServiceImpl;
 
 public class AppUpgraded extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
-        // Restart the app on auto updates
-        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
-        context.startActivity(launchIntent);
+        // Restart the location service on update.
+        Intent locationServiceIntent = new Intent(context, LocationServiceImpl.class);
+        locationServiceIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(locationServiceIntent);
+        } else {
+            context.startService(locationServiceIntent);
+        }
     }
 }
