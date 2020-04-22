3e5196519dddce559ddf0199f0e5f047e83c9077
Pablo Anaya <pjanaya@gmail.com>
2020-04-22 23:46:32 +0000

    Fixed center positioning. Added RTL support.

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components/native';
 import { scale } from '../../utils/index';
 import Colors from '../../constants/Colors';
 import { StyleProp, TextStyle } from 'react-native';
+import { isRTL } from '../../i18n';
 
 interface FontProps {
   marginBottom?: 0 | 0.25 | 0.5 | 1 | 1.5 | 2;
@@ -31,17 +32,17 @@ const font = (
   font-size: ${scale(size)};
   line-height: ${scale(size * lineHeight)};
   margin-bottom: ${scale(size * lineHeight * bottomMargin)};
-  text-align: ${center ? 'center' : 'left'};
+  text-align: ${center ? 'center' : isRTL() ? 'right' : 'left'};
 `;
 
 export const Paragraph = styled.Text<FontProps & { bold?: boolean }>`
-  ${({ marginBottom }) => font(15, 1.5, marginBottom)};
+  ${({ marginBottom, center }) => font(15, 1.5, marginBottom, center)};
   font-family: 'OpenSans${({ bold }) => (bold ? 'Bold' : '')};
 `;
 
 export const Heading = styled.Text<FontProps & { level: Level }>`
-  ${({ marginBottom, level = 1 }) =>
-    font(headingSize[level], 1.2, marginBottom)};
+  ${({ marginBottom, level = 1, center }) =>
+    font(headingSize[level], 1.2, marginBottom, center)};
   font-weight: 900;
   font-family: 'OpenSansExtraBold';
 
