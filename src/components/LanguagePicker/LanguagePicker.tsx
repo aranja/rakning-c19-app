9482cd3dd24de554bed196d00f5498d2cf759072
Pablo Anaya <pjanaya@gmail.com>
2020-04-29 23:10:15 +0000

    Removed title from LanguagePicker

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -8,25 +8,19 @@ import {
 } from '../../i18n/index';
 import Colors from '../../constants/Colors';
 import { CtaButton } from '../Button/index';
-import { verticalScale, scale } from '../../utils/index';
 import { withNavigation } from 'react-navigation';
 import { Content } from '../AppShell';
 import { useTranslation } from 'react-i18next';
 
 const LanguageScreen = styled.View``;
 
-const Title = styled(Content)`
-  height: ${verticalScale(150)};
-`;
-
 const LanguageList = styled(Content)``;
 
 interface Props {
-  showTitle?: boolean;
   onLanguagePress?: () => void;
 }
 
-function LanguagePicker({ showTitle, onLanguagePress }: Props) {
+function LanguagePicker({ onLanguagePress }: Props) {
   const { t, i18n } = useTranslation();
   const [locale, setLocale] = React.useState(i18n.language);
 
@@ -47,22 +41,6 @@ function LanguagePicker({ showTitle, onLanguagePress }: Props) {
 
   return (
     <LanguageScreen>
-      {showTitle && (
-        <Title>
-          <Text type="heading" level={2} marginBottom={0.25}>
-            {t('welcomeTitle')}
-          </Text>
-          <Text
-            style={{
-              fontSize: verticalScale(16),
-              lineHeight: verticalScale(16 * 1.4),
-            }}
-          >
-            {t('languageDescription')}
-          </Text>
-        </Title>
-      )}
-
       <LanguageList>
         {languages
           .sort((a, b) => langSorter(a, b))
